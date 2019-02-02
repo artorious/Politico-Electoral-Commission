d643802e1a02e86a900cff92a8dd3b23f84f61a2
Arthur Ngondo <ngondoarthur@gmail.com>
2019-02-02 05:51:10 +0300

    [feature #163534367] Add voting portal

---
+++ UI/static/main.js
@@ -0,0 +1,44 @@
+
+function candidates_per_position() {
+    location.replace("candidates_per_position.html")
+}
+
+function confirm_presidential_vote() {
+   if (confirm("Are you sure of your choice for president? Press O.K to continue.")) {
+        alert('Vote for president cast!')
+   }
+}
+
+function confirm_skipping_prez_vote() {
+    if (confirm("Are you sure you want to skip presidential vote? Press O.K to continue.")) {
+        alert('Pesidential Vote skipped!')
+        location.replace("gubernatorial_vote.html")
+    }
+ }
+
+function confirm_gubenatorial_vote() {
+   if (confirm("Are you sure of your choice for governer? Press O.K to continue.")) {
+        alert('Vote for Governer cast.')
+   }
+}
+
+function confirm_skipping_gov_vote() {
+    if (confirm("Are you sure you want to skip gubernatorial vote? Press O.K to continue.")) {
+        alert('Vote for Governor skipped! All Done. Thank you.')
+        location.replace("senatorial_vote.html")
+    }
+ }
+
+
+function confirm_senatorial_vote() {
+   if (confirm("Are you sure of your choice for Senator? Press O.K to continue.")) {
+        alert('Vote for Senator cast. All done, Thank You')
+   }
+}
+
+function confirm_skipping_sen_vote() {
+    if (confirm("Are you sure you want to skip senatorial vote? Press O.K to continue.")) {
+        alert('Vote for Senator skipped! All Done. Thank you.')
+        location.replace("user_profile.html")
+    }
+ }
